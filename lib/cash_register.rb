fd43102d66ceeabc42b0814285815f1e09dcdd85
Karl Benbeneck <kbenbeneck@gmail.com>
2019-10-24 00:30:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,26 +2,10 @@ class CashRegister
 
 
   def initialize(discount=0)
-    self.total = 0
-    self.discount = discount
-    self.items = []
-    self.last_tansaction_price = 0
-  end
-
-  def total=(value)
-    @total = value
-  end
-
-  def discount=(value)
-    @discount = value
-  end
-
-  def discount
-    @discount
-  end
-
-  def total
-    @total
+    @total = 0
+    @discount = discount
+    @items = []
+    @last_tansaction_price = 0
   end
 
   def add_item(title, price, quantity=1)
